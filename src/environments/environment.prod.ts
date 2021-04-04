c9f97f2ee837fdc82262d054fbaf90b9119a6636
AlexJRV <alexisfserrano07@gmail.com>
2021-04-04 20:46:51 -0300

    Se agregó Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDpyMDGZP9iRGAeqQR6SPRPNEdk2ODMIos",
+    authDomain: "monicamarykay-b5266.firebaseapp.com",
+    databaseURL: "https://monicamarykay-b5266-default-rtdb.firebaseio.com",
+    projectId: "monicamarykay-b5266",
+    storageBucket: "monicamarykay-b5266.appspot.com",
+    messagingSenderId: "486291174060",
+    appId: "1:486291174060:web:d964a0b7a8284885ffa509",
+    measurementId: "G-GQDQZ5B0BR"
+  }
 };
